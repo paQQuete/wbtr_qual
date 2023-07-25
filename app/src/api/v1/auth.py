2968e6ffb871d88d955f88aa08608326ae2a08d3
boris <boris.vladimirov13@gmail.com>
2023-07-25 23:45:08 +0600

    handle jwt in fastapi

--- app/src/api/v1/auth.py
+++ app/src/api/v1/auth.py
@@ -1,17 +1,21 @@
 from http import HTTPStatus
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, HTTPException, Form
+from fastapi import APIRouter, Depends, HTTPException, Form, Body
+from fastapi.security import OAuth2PasswordBearer
+from redis.asyncio.client import Redis
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from models.models import HTTPErrorDetails
 from models.schemas.auth import UserCreate, User
 from db.database import get_db
+from db.redis_inj import get_redis
 from services.crud.users import create_user, get_user_by_email, get_user_by_username
 from services.password import get_hashed_password, verify_password
-from services.jwt import create_access_token, create_refresh_token
+from services.jwt import create_access_token, create_refresh_token, JWTBearer, blacklisting
 
 router = APIRouter()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 @router.post('/signup', response_model=User)
@@ -31,3 +35,36 @@ async def login_user(email: Annotated[str, Form()], password: Annotated[str, For
     if not verify_password(password=password, hashed_pass=user.password_hash):
         raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail=HTTPErrorDetails.BAD_REQUEST.value)
     return create_access_token(subject=user.id), create_refresh_token(subject=user.id)
+
+
+@router.post('/just_check')
+async def check_token(token=Depends(JWTBearer())):
+    return token
+
+
+@router.post('/logout')
+async def logout(refresh_token: str, access_token=Depends(JWTBearer()), redis: Redis = Depends(get_redis)):
+    await blacklisting(redis=redis, token_or_jti=refresh_token)
+    await blacklisting(redis=redis, token_or_jti=access_token)
+    return HTTPStatus.OK
+
+
+@router.post('/logout_debug')
+async def logout_debg(refresh_token: Annotated[str, Body(embed=True)], access_token: Annotated[str, Depends(oauth2_scheme)],
+                      redis: Redis = Depends(get_redis)):
+    await blacklisting(redis=redis, token_or_jti=refresh_token)
+    await blacklisting(redis=redis, token_or_jti=access_token)
+    # TODO
+    #  File "/home/boris/PycharmProjects/wbtr_qual/app/src/api/v1/auth.py", line 56, in logout_debg
+    #     await blacklisting(redis=redis, token_or_jti=access_token)
+    #   File "/home/boris/PycharmProjects/wbtr_qual/app/src/services/jwt.py", line 74, in blacklisting
+    #     token = jwt.decode(
+    #             ^^^^^^^^^^^
+    #   File "/home/boris/PycharmProjects/wbtr_qual/venv/lib/python3.11/site-packages/jose/jwt.py", line 157, in decode
+    #     _validate_claims(
+    #   File "/home/boris/PycharmProjects/wbtr_qual/venv/lib/python3.11/site-packages/jose/jwt.py", line 481, in _validate_claims
+    #     _validate_exp(claims, leeway=leeway)
+    #   File "/home/boris/PycharmProjects/wbtr_qual/venv/lib/python3.11/site-packages/jose/jwt.py", line 314, in _validate_exp
+    #     raise ExpiredSignatureError("Signature has expired.")
+    # jose.exceptions.ExpiredSignatureError: Signature has expired.
+    return HTTPStatus.OK
--- app/src/services/jwt.py
+++ app/src/services/jwt.py
@@ -5,6 +5,8 @@ from typing import Union, Any
 
 from jose import jwt
 from redis.asyncio.client import Redis
+from fastapi import Request, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
 from core.config import SETTINGS
 
@@ -25,9 +27,9 @@ def _create_token():
             to_encode = {
                 "exp": exp_delta,
                 "sub": str(kwargs['subject']),
-                "iss": SETTINGS.PROJECT,
+                "iss": SETTINGS.PROJECT.PROJECT_NAME,
                 "nbf": datetime.utcnow(),
-                "jti": uuid.uuid4(),
+                "jti": str(uuid.uuid4()),
                 "iat": datetime.utcnow()
             }
             encoded_jwt = jwt.encode(
@@ -69,14 +71,15 @@ def create_refresh_token(subject: Union[str, Any], expires_delta: int = None, _e
 
 async def blacklisting(redis: Redis, token_or_jti: Union[uuid.UUID, str]) -> bool:
     if isinstance(token_or_jti, str):
-        await redis.set(
-            name=(token := jwt.decode(
+        token = jwt.decode(
                 token_or_jti,
                 key=SETTINGS.JWT.SECRET_KEY,
                 algorithms=SETTINGS.JWT.ALGORITHM
-            )['payload']['jti']),
-            value=True,
-            exat=token['payload']['exp'] - str(datetime.utcnow().timestamp())
+            )
+        await redis.set(
+            name=token['jti'],
+            value=1,
+            exat=token['exp'] - int(datetime.utcnow().timestamp())
         )
         return True
     elif isinstance(token_or_jti, uuid.UUID):
@@ -88,8 +91,33 @@ async def check_blacklist(redis: Redis, token_or_jti: Union[uuid.UUID, str]) -> 
     if isinstance(token_or_jti, str):
         bltoken = await redis.get(name=jwt.decode(token_or_jti,
                                                   key=SETTINGS.JWT.SECRET_KEY,
-                                                  algorithms=SETTINGS.JWT.ALGORITHM)['payload']['jti'])
+                                                  algorithms=SETTINGS.JWT.ALGORITHM)['jti'])
         return True if bltoken else False
     elif isinstance(token_or_jti, uuid.UUID):
         bltoken = await redis.get(name=token_or_jti)
         return True if bltoken else False
+
+
+class JWTBearer(HTTPBearer):
+    def __init__(self, auto_error: bool = True):
+        super(JWTBearer, self).__init__(auto_error=auto_error)
+
+    async def __call__(self, request: Request):
+        credentials: HTTPAuthorizationCredentials = await super(JWTBearer, self).__call__(request)
+        if credentials:
+            if not credentials.scheme == "Bearer":
+                raise HTTPException(status_code=403, detail="Invalid authentication scheme.")
+            if not self.verify_jwt(credentials.credentials):
+                raise HTTPException(status_code=403, detail="Invalid token or expired token.")
+            return credentials.credentials
+        else:
+            raise HTTPException(status_code=403, detail="Invalid authorization code.")
+
+    def verify_jwt(self, jwtoken: str) -> bool:
+        try:
+            payload = jwt.decode(jwtoken,
+                                 key=SETTINGS.JWT.SECRET_KEY,
+                                 algorithms=SETTINGS.JWT.ALGORITHM)
+        except:
+            payload = None
+        return True if payload and payload['exp'] >= int(datetime.utcnow().timestamp()) else False
